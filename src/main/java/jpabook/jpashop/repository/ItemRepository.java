b3bdf76a1927407e43a66694f0e5826e2fcb6d23
taeik <kti1452@naver.com>
2026-01-30 06:25:49 +0900

    feat: Implement item update

--- src/main/java/jpabook/jpashop/repository/ItemRepository.java
+++ src/main/java/jpabook/jpashop/repository/ItemRepository.java
@@ -15,8 +15,6 @@ public class ItemRepository {
     public void save(Item item) {
         if (item.getId() == null)
             em.persist(item);
-        else
-            em.merge(item);
     }
 
     public Item findOne(Long id) {
