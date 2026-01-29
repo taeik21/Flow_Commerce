3582e519026a85f301a9aefb3bdf1b711c0108c4
taeik <kti1452@naver.com>
2026-01-29 19:10:26 +0900

    feat: Implement member list view

--- src/main/java/jpabook/jpashop/controller/MemberController.java
+++ src/main/java/jpabook/jpashop/controller/MemberController.java
@@ -41,5 +41,10 @@ public class MemberController {
         return "redirect:/";
     }
 
-
+    @GetMapping("/members")
+    public String list(Model model) {
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
+        return "members/memberList";
+    }
 }
